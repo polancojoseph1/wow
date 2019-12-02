51f8d5dbd99c711f0057821ee912ad435ef992ec
John McDonald <johnptmcdonald@gmail.com>
2019-12-02 12:28:49 -0500

    Initial commit

---
+++ app/components/root.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Root = () => {
+  return (
+    <div>
+      <nav>
+        Welcome!
+      </nav>
+      <main>
+        <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
+        <p>This seems like a nice place to get started with some Routes!</p>
+      </main>
+    </div>
+  )
+}
+
+export default Root
